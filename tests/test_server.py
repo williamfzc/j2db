dd1a2feba7917b4b04b0345ac67e6b6bb49d3477
williamfzc <178894043@qq.com>
2019-09-12 18:46:48 +0800

    chore(ci): add mysql server to workflow

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,15 +2,36 @@ import requests
 import pytest
 import time
 from multiprocessing import Process
+from sqlalchemy import Column, String, Integer
 
-from json2db.server import start_server
+from json2db.server import Server
+from json2db.server import BaseModel
+from json2db.db import MySQLManager
 
 URL_PREFIX = r"http://127.0.0.1:9410"
 
 
+class SomeModel(BaseModel):
+    __tablename__ = "some_table"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(64), unique=True)
+
+
 @pytest.fixture(scope="module", autouse=True)
 def my_fixture():
-    p = Process(target=start_server, args=(9410,))
+    manager = MySQLManager(
+        url="127.0.0.1",
+        port=3306,
+        user="user",
+        password="pwd",
+        db_name="some_test",
+    )
+    manager.connect()
+
+    s = Server()
+    s.init_db(manager)
+    p = Process(target=s.start)
     p.start()
     time.sleep(3)
     yield
