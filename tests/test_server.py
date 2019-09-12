4a36047cd9ab7b810a09c0a8ed6bef221d47622d
williamfzc <178894043@qq.com>
2019-09-12 14:25:38 +0800

    chore(test): add some test cases

--- tests/test_server.py
+++ tests/test_server.py
@@ -5,6 +5,8 @@ from multiprocessing import Process
 
 from json2db.server import start_server
 
+URL_PREFIX = r'http://127.0.0.1:9410'
+
 
 @pytest.fixture(scope="module", autouse=True)
 def my_fixture():
@@ -16,5 +18,29 @@ def my_fixture():
 
 
 def test_hello():
-    resp = requests.get('http://127.0.0.1:9410/')
+    resp = requests.get(f'{URL_PREFIX}/')
     assert resp.ok
+
+
+def test_form_json_invalid():
+    resp = requests.post(
+        f'{URL_PREFIX}/api/json/form',
+        data={
+            'tag': 'abc',
+            'content': 'is{}#@!$'
+        }
+    )
+    assert resp.ok
+    assert resp.json()['error'] == 'json_invalid'
+
+
+def test_params_json_invalid():
+    resp = requests.post(
+        f'{URL_PREFIX}/api/json/params',
+        json={
+            'tag': 'abc',
+            'content': 'is{}#@!$'
+        }
+    )
+    assert resp.ok
+    assert resp.json()['error'] == 'json_invalid'
